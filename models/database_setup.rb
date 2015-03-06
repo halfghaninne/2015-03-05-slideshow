a7cf6bba87225faa3ab20711c8678bef98aec192
Alexandra Millatmal <alexandrajmh@gmail.com>
2015-03-06 00:09:30 -0600

    populated the database with a couple of records

--- models/database_setup.rb
+++ models/database_setup.rb
@@ -3,6 +3,6 @@ require 'sqlite3'
 DATABASE.results_as_hash = true
 
 DATABASE.execute("CREATE TABLE IF NOT EXISTS slides
-                  (order INTEGER PRIMARY KEY,
+                  (id INTEGER PRIMARY KEY,
                   title TEXT, 
-                  body_text TEXT)"
+                  body TEXT)"
--- models/slide.rb
+++ models/slide.rb
@@ -1,20 +1,18 @@
 require 'pry'
 require 'sqlite3'
 
-DATABASE = SQLite3::Database.new("slideshow.db")
-
-DATABASE.results_as_hash = true
-
-DATABASE.execute("CREATE TABLE IF NOT EXISTS slides
-                  (order INTEGER PRIMARY KEY,
-                  title TEXT, 
-                  body_text TEXT)"
+# DATABASE = SQLite3::Database.new("slideshow.db")
+#
+# DATABASE.results_as_hash = true
+#
+# DATABASE.execute("CREATE TABLE IF NOT EXISTS slides (id INTEGER PRIMARY KEY,
+#                   title TEXT, body TEXT)")
 
 class Slide
   
-  attr_reader :order
+  attr_reader :id
   
-  attr_accessor :title, :body_text 
+  attr_accessor :title, :body 
   
   def initialize(options)
     @title = options["title"]
@@ -23,17 +21,17 @@ class Slide
   
   def insert
     DATABASE.execute("INSERT INTO slides (title, body) 
-                      VALUES ('#{@text}', '#{@body})")
-    @order = DATABASE.last_insert_row_id
+                      VALUES ('#{@title}', '#{@body}')")
+    @id = DATABASE.last_insert_row_id
   end
   
-  def edit(order, field, value)
+  def edit(id, field, value)
     DATABASE.execute("UPDATE slides SET '#{field}' = '#{value}' 
-                      WHERE order = '#{order}'")
+                      WHERE id = '#{id}'")
   end
   
   def find(order)
-    result = DATABASE.execute("SELECT * FROM slides WHERE order = #{order}").[0]
+    result = DATABASE.execute("SELECT * FROM slides WHERE id = #{id}")[0]
     
     self.new(result)
   end
@@ -46,13 +44,13 @@ class Slide
   
   def to_hash
     {
-      order: order,
+      id: id,
       title: title,
       body: body,
     }
   end
   
   def self.delete(order)
-    DATABASE.execute("DELETE FROM slides where order = #{order}")
+    DATABASE.execute("DELETE FROM slides where id = #{id}")
   end
 end
